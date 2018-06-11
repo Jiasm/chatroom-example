9a83de65797637cf34b18b8db59d51c1382f991d
jiashunming <jiashunming@outlook.com>
2018-06-11 09:18:18 +0800

    update: add mysql config

--- conf/index.js
+++ conf/index.js
@@ -6,5 +6,11 @@ const dirname = path.dirname(process.mainModule.filename)
 
 export default {
   port: 12306,
+  mysql: {
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'jarvis',
+    database: 'chatroom_example'
+  },
   logRoot: path.resolve(dirname, './logs')
 }
